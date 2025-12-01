51eeb1aaeb794847d2d180a5e453762cd3314e46
01011001andra <yandrayt1@gmail.com>
2025-12-02 06:43:01 +0700

    second update

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButton } from "../primary-button/primary-button";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButton],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-  title = 'this is header';
-  number = signal(0);
+  isLogin = signal(true);
 
-  increment() {
-    this.number.update((v) => v + 1);
+  handleClick(type: 'cart') {
+    if (type == 'cart') {
+      console.log('Cart Clicked');
+    }
   }
 }
